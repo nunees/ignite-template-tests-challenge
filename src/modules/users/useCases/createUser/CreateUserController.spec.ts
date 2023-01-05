628ab832da867d74917ab51cf589884320d2aef3
nunees <felipe_silva_nunes@hotmail.com>
2023-01-05 20:20:46 -0300

    test: add user controller test

---
+++ src/modules/users/useCases/createUser/CreateUserController.spec.ts
@@ -0,0 +1,56 @@
+import { Connection } from "typeorm";
+import request  from "supertest";
+import {hash} from 'bcrypt';
+import {v4 as uuid} from 'uuid';
+
+import {app} from '../../../../app';
+import createConnection from '../../../../database';
+
+
+let connection: Connection;
+
+describe("Create user controller", () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it("Should be able to create a new user", async () => {
+    const response = await request(app)
+      .post('/api/v1/users')
+      .send({
+        name: "User test",
+        email: "user@email.com",
+        password: "senhasecreta"
+      });
+
+      expect(response.status).toBe(201);
+  });
+
+  it("Should not be able to create a new user with same email", async() => {
+      await request(app)
+      .post('/api/v1/users')
+      .send({
+        name: "User test",
+        email: "user@email.com",
+        password: "senhasecreta"
+      });
+
+      const response = await request(app)
+      .post('/api/v1/users')
+      .send({
+        name: "User test",
+        email: "user@email.com",
+        password: "senhasecreta"
+      });
+
+
+      expect(response.status).toBe(400);
+      expect(response.body.message).toEqual("User already exists");
+  })
+});
